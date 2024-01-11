1457f45621311016f8f73cdb7169ac95857bb543
absillers <71862407+absillers@users.noreply.github.com>
2024-01-11 18:34:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const config = {
   zoom: 6,
   title: 'Program List',
   description:
-    'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
+    'TO REPLACE',
   sideBarInfo: ['School'],
   popupInfo: ['School'],
   filters: [
