248d82cf5300f8221f26b25c80b77ee390977fde
absillers <71862407+absillers@users.noreply.github.com>
2024-01-11 23:56:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const config = {
     {
       type: 'dropdown',
       title: 'Programs Offered: ',
-      columnHeader: 'Languages',
+      columnHeader: 'Programslist',
       listItems: [
         'Amharic',
         'ASL',
