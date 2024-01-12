b11bbaed35b5fe080149df19aba8100f493a10ca
absillers <71862407+absillers@users.noreply.github.com>
2024-01-11 21:36:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 'use strict';
 
-
 // eslint-disable-next-line no-unused-vars
 const config = {
-  style:  'mapbox://styles/mapbox/streets-v11',,
+  style: 'mapbox://styles/mapbox/light-v10',
   accessToken:
-    'pk.eyJ1IjoiYW5uYXNpbGxlcnMiLCJhIjoiY2xweDZ2a2RrMGk1ZjJqbnJnc25pZGhyYSJ9.0kwnSzA5FG6Cmse2aBC4AQ',
-  CSV: './sample_data_og.csv',
+    'pk.eyJ1IjoibWFwYm94LWNvbW11bml0eSIsImEiOiJja2tkN21jcjAwMG51MnBxdHAxemdueGpzIn0.e0IzLkytGq4pcGGieP8KNA',
+  CSV: './Sample_Data.csv',
   center: [-120.234, 47.398],
   zoom: 6,
   title: 'Replace with your title',
   description:
     'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
-  sideBarInfo: ['School'],
-  popupInfo: ['School'],
+  sideBarInfo: ['Location_Name', 'Address', 'Phone'],
+  popupInfo: ['Location_Name'],
   filters: [
     {
       type: 'dropdown',
