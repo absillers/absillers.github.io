eaa88e608bc4db13ad5430d47c21b58ac9171fb6
absillers <71862407+absillers@users.noreply.github.com>
2024-01-11 19:10:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line no-unused-vars
 const config = {
-  style: 'mapbox://styles/mapbox/navigation-night-v1',
+  style: 'mapbox://styles/mapbox/streets-v11',
   accessToken:
     'pk.eyJ1IjoiYW5uYXNpbGxlcnMiLCJhIjoiY2xweDZ2a2RrMGk1ZjJqbnJnc25pZGhyYSJ9.0kwnSzA5FG6Cmse2aBC4AQ',
   CSV: './Sample_Data.csv',
